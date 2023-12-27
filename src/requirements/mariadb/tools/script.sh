d3122abe7558866c45917732b53afaa99461de10
Ayoub Mentag <amentag@e2r9p8.1337.ma>
2023-12-27 17:53:03 +0100

    last commit

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
-# Start MySQL in the background
+set -e  # Exit on error
+
+# Start MariaDB in the background
 mysqld_safe &
 
-# Wait for MySQL to be ready
+# Wait for MariaDB to be ready
 while ! mysqladmin ping --silent; do
     sleep 1
-    echo "Waiting for MySQL to be ready..."
+    echo "Waiting for MariaDB to be ready..."
 done
 
 # Create SQL script file
-echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" > /tmp/file.sql
-echo "CREATE USER 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" >> /tmp/file.sql
-echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> /tmp/file.sql
-echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> /tmp/file.sql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' with grant option;" >> /tmp/file.sql
-echo "FLUSH PRIVILEGES;" >> /tmp/file.sql
+SQL_SCRIPT="/tmp/file.sql"
+echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" > "$SQL_SCRIPT"
+echo "CREATE USER 'root'@'%' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" >> "$SQL_SCRIPT"
+echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> "$SQL_SCRIPT"
+echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';" >> "$SQL_SCRIPT"
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' with grant option;" >> "$SQL_SCRIPT"
+echo "FLUSH PRIVILEGES;" >> "$SQL_SCRIPT"
 
 # Print the content of the SQL script for debugging
 echo "SQL Script:"
-cat /tmp/file.sql
+cat "$SQL_SCRIPT"
 
 # Execute the SQL script
 echo "Executing SQL script..."
-mysql -u root -p"$SQL_ROOT_PASSWORD" < /tmp/file.sql
+mysql -u root -p"$SQL_ROOT_PASSWORD" < "$SQL_SCRIPT"
 
-# Shutdown MySQL
-echo "Shutting down MySQL..."
+# Shutdown MariaDB
+echo "Shutting down MariaDB..."
 mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown
 
-# Start MySQL again
-echo "Starting MySQL again..."
-mysqld_safe &
+# Clean up
+rm "$SQL_SCRIPT"
+
+# Start MariaDB again
+echo "Starting MariaDB again..."
+mysqld_safe
--- src/requirements/wordpress/tools/script.sh
+++ src/requirements/wordpress/tools/script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sleep 20
+
 cd /var/www/html
 
 sed -i 's/^listen = .*/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
@@ -10,9 +12,27 @@ fi
 
 # wp core download  --allow-root
 if [ ! -e wp-config.php ]; then
-    wp config create --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=$DB_HOST --allow-root --path="/var/www/html"
+    echo "Accessing to DB ...[wordpress]" 
+    # wp config create --dbname=$SQL_DATABASE \
+    #                  --dbuser=$SQL_USER \
+    #                  --dbpass=$SQL_PASSWORD \
+    #                  --dbhost=$DB_HOST \
+    #                  --allow-root
+
+    wp config set DB_HOST "mariadb" --allow-root
+    wp config set DB_NAME "amentagdb" --allow-root
+    wp config set DB_USER "amentag" --allow-root
+    wp config set DB_PASSWORD "1234" --allow-root
+    echo "create the config file "
 fi
 
-wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$ADMIN_USER --skip-email --allow-root --admin_password=$ADMIN_PASSWD --admin_email=$ADMIN_EMAIL --path="/var/www/html"
+wp core install --url=$DOMAIN_NAME \
+				--title=$TITLE \
+				--admin_user=$ADMIN_USER \
+                --skip-email \
+				--allow-root \
+				--admin_password=$ADMIN_PASSWD \
+				--admin_email=ADMIN_EMAIL --path="/var/www/html"
+
 
 /usr/sbin/php-fpm7.4 -F
