94ffd340c5402895f33d56576d413b32961a9936
Ayoub Mentag <amentag@e2r3p11.1337.ma>
2023-12-30 13:25:57 +0100

    502

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
 
 build:
 	docker-compose -f ./srcs/docker-compose.yml up --build
+
+fclean :
+	docker system prune -a
+	docker volume prune --force
+	rm -rf ~/wordpress/*
+	rm -rf ~/wordpress/*
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,10 +6,6 @@ apt install -y sudo
 apt install -y wget
 apt install -y tar
 
-
-
-
-
 # while ! mysqladmin -h"localhost" -P"3306" status; do
 #     sleep 1
 #     echo "waiting for MariaDB to start ..."
@@ -25,12 +21,13 @@ cd /var/www/html
 wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 
+
 # Configure WordPress
 cp wordpress/wp-config-sample.php wordpress/wp-config.php
-sed -i "s/database_name_here/$DB_NAME/" wordpress/wp-config.php
-sed -i "s/username_here/$USER_NAME/" wordpress/wp-config.php
-sed -i "s/password_here/$USER_PASS/" wordpress/wp-config.php
-
+sed -i "s/database_name_here/amentagdb/" wordpress/wp-config.php
+sed -i "s/username_here/amentag/" wordpress/wp-config.php
+sed -i "s/password_here/Normal/" wordpress/wp-config.php
+sed -i "s/localhost/mariadb/" wordpress/wp-config.php
 if ! command -v wp &> /dev/null
 then
     echo "Installing WP-CLI..."
@@ -40,17 +37,17 @@ then
     echo "WP-CLI installed."
 fi
 
-# Install WordPress
-wp core install --url="amentag.1337.ma" --title="$WEB_TITLE" --admin_user="$ADMIN_WP" --skip-email --admin_password="$ADMIN_PASS" --admin_email="$ADMIN_EMAIL" --path="./wordpress" --allow-root
-
-# Add Admin User
-wp user create "$ADMIN_USER" "$ADMIN_EMAIL" --role="administrator" --user_pass="$ADMIN_PASSWORD" --path="./wordpress" --allow-root
+#install wp core [REMOVE --skip emain]
+wp core install --url="amentag.1337.ma" --title="WebTitle" --admin_user="Ayoub" --admin_password="Secret" --admin_email="admin@example.com" --skip-email --path="./wordpress" --allow-root
 
 # Add Normal User
-wp user create "$WP_USER" "$WP_EMAIL" --role="$WP_ROLE" --user_pass="$WP_PASS" --path="./wordpress" --allow-root
+wp user create "normal-user" "normal@example.com" --role="author" --user_pass="Normal" --path="./wordpress" --allow-root
 
 # Clean up (remove downloaded WordPress archive)
 rm latest.tar.gz
+
+chmod 777 wordpress/*
+chown -R www-data:www-data /var/www/html/wordpress
 
 # Starting the PHP FastCGI
 /usr/sbin/php-fpm7.4 -F
