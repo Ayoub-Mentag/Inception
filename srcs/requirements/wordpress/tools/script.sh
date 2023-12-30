fbca034eca853b2208ac66aec1722e4378945e1d
Ayoub Mentag <amentag@e2r3p11.1337.ma>
2023-12-30 15:01:18 +0100

    finally 2

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,6 +6,9 @@ apt install -y sudo
 apt install -y wget
 apt install -y tar
 
+
+sed -i 's/^listen = .*/listen = wordpress:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+
 # while ! mysqladmin -h"localhost" -P"3306" status; do
 #     sleep 1
 #     echo "waiting for MariaDB to start ..."
@@ -13,13 +16,17 @@ apt install -y tar
 
 sleep 20
 
-echo "CONNECTED"
+echo "CONNECTED"listen
 
 cd /var/www/html
 
-# Download and extract WordPress
-wget https://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
+# # Download and extract WordPress
+# wget https://wordpress.org/latest.tar.gz
+# tar -xzvf latest.tar.gz
+
+
+# # Download WordPress
+wp core download --allow-root
 
 
 # Configure WordPress
@@ -28,14 +35,6 @@ sed -i "s/database_name_here/amentagdb/" wordpress/wp-config.php
 sed -i "s/username_here/amentag/" wordpress/wp-config.php
 sed -i "s/password_here/Normal/" wordpress/wp-config.php
 sed -i "s/localhost/mariadb/" wordpress/wp-config.php
-if ! command -v wp &> /dev/null
-then
-    echo "Installing WP-CLI..."
-    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-    chmod +x wp-cli.phar
-    sudo mv wp-cli.phar /usr/local/bin/wp
-    echo "WP-CLI installed."
-fi
 
 #install wp core [REMOVE --skip emain]
 wp core install --url="amentag.1337.ma" --title="WebTitle" --admin_user="Ayoub" --admin_password="Secret" --admin_email="admin@example.com" --skip-email --path="./wordpress" --allow-root
